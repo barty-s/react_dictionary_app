5b15094fa77fb125a884397decbdea3ec622f7ce
barty-s <117319197+barty-s@users.noreply.github.com>
2023-01-25 14:57:40 +0100

    Project set up

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+      </header>
+      <footer>
+        Built by{" "}
+        <a
+          href="https://www.linkedin.com/in/sarah-barter-6383b196/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sarah Barter
+        </a>{" "}
+        and open-sourced on{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
