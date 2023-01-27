dc4c7f98b59c698b94d6a4ac2d8bede8bd784d7e
barty-s <117319197+barty-s@users.noreply.github.com>
2023-01-27 12:10:09 +0100

    Integrated dictionary API with axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleSearchResponse(response) {
+    console.log(response.data[0]);
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -11,6 +17,9 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleSearchResponse);
   }
 
   return (
