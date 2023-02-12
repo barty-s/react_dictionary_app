9e209f009921d5f23d844e52164159dc7b91403f
barty-s <117319197+barty-s@users.noreply.github.com>
2023-02-12 13:41:34 +0100

    Added photos component and API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleSearchResponse(response) {
     setResults(response.data[0]);
@@ -15,11 +17,21 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleSearchResponse);
+
+    let pexelsApiKey =
+      "ZP90cJ165CKbmnWG0mdx0m6Z1vRerU7GPnJVqxczNXSOVIqHinp8PeKv";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   return (
@@ -35,6 +47,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
